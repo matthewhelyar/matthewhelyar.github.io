97bc505753cf80708c6e23a2cce720ad6a648275
Matthew Helyar <62142581+matthewhelyar@users.noreply.github.com>
2023-01-29 09:57:57 +0000

    input masking din

--- blood/sidebar/DinForm2.js
+++ blood/sidebar/DinForm2.js
@@ -4,20 +4,72 @@ class DinForm2 {
 		this.yearIn = yearInput;
 		this.seqIn = seqInput;
 
+		this.finIn.addEventListener('keydown', this.onKeyDown.bind(this, event));
 		this.finIn.addEventListener('keyup', this.validateFin.bind(this, event));
 		this.finIn.addEventListener('focusout', this.finalValidate.bind(this));
 		this.finIn.addEventListener('focusin', this.selectSingleChar.bind(event));
 		this.finIn.addEventListener('click', this.selectSingleChar.bind(event));
 
+		this.yearIn.addEventListener('keydown', this.onKeyDown.bind(this, event));
 		this.yearIn.addEventListener('keyup', this.validateYear.bind(this, event));
 		this.yearIn.addEventListener('focusout', this.finalValidate.bind(this));
 		this.yearIn.addEventListener('focusin', this.selectSingleChar.bind(event));
 		this.yearIn.addEventListener('click', this.selectSingleChar.bind(event));
 
+		this.seqIn.addEventListener('keydown', this.onKeyDown.bind(this, event));
 		this.seqIn.addEventListener('keyup', this.validateSeq.bind(this, event));
 		this.seqIn.addEventListener('focusout', this.finalValidate.bind(this));
 		this.seqIn.addEventListener('focusin', this.selectSingleChar.bind(event));
 		this.seqIn.addEventListener('click', this.selectSingleChar.bind(event));
+	}
+
+	onKeyDown(_discard, event) {
+
+		// try input masking here.
+		const t = event.target;
+		const l = t.length;
+		let s = t.value;
+		let c = { start: t.selectionStart, end: t.selectionEnd };
+		let char = event.key; //String.fromCharCode(event.keyCode);
+		let code = event.keyCode;
+
+		//console.log(code, char);
+
+		if ((code >= 48 && code <= 57) && !event.shiftKey) {
+			//console.log("0-9");
+		}
+		else if (code >= 65 && code <= 90) {
+			//console.log("A-Z");
+			if (event.target != this.finIn || c.start >= 3)
+				event.preventDefault();
+		}
+		else if (code >= 97 && code <= 122) {
+			//console.log("a-z");
+			if (event.target != this.finIn || c.start >= 3)
+				event.preventDefault();
+		}
+		else if (code == 8) {
+			//console.log("backspace");
+		}
+		else if (code == 9) {
+			//console.log("tab");
+		}
+		else if (code == 27) {
+			//console.log("escape");
+		}
+		else if (code >= 37 && code <= 40) {
+			//console.log("arrow keys");
+			// can't change the selection in here.
+			//this.handleArrowKeys(event, null, this.yearIn);
+		}
+		//else if (code == 45) console.log("insert");
+		else if (code == 46) {
+			//console.log("delete");
+		}
+		else {
+			//console.log("invalid char");
+			event.preventDefault(); // don't insert char
+		}
 	}
 
 	selectSingleChar(event) {
@@ -36,19 +88,15 @@ class DinForm2 {
 	handleArrowKeys(event, prev, next) {
 		// returns true if arrow key so can avoid input validation for this keystroke which messes up caret position.
 		function gotoPrev() {
-			if (prev) {
-				prev.focus();
-				prev.selectionStart = prev.value.length - 1;
-				prev.selectionEnd = prev.value.length;
-			}
+			prev.focus();
+			prev.selectionStart = prev.value.length - 1;
+			prev.selectionEnd = prev.value.length;
 		}
 
 		function gotoNext() {
-			if (next) {
-				next.focus();
-				next.selectionStart = 0;
-				next.selectionEnd = 1;
-			}
+			next.focus();
+			next.selectionStart = 0;
+			next.selectionEnd = 1;
 		}
 
 		let t = event.target;
@@ -65,28 +113,32 @@ class DinForm2 {
 			case 37:
 				// left
 				if (t.selectionStart == 0) {
-					gotoPrev();
-					return true;
+					if (prev) {
+						gotoPrev();
+						return true;
+					}
 				}
 				t.selectionEnd = Math.max(t.selectionStart, 1);
 				t.selectionStart = Math.max(t.selectionStart - 1, 0);
 				return true;
 			case 38:
 				// up
-				gotoPrev();
+				if (prev) gotoPrev();
 				return true;
 			case 39:
 				// right
 				if (t.selectionStart == t.value.length) {
-					gotoNext();
-					return true;
+					if (next) {
+						gotoNext();
+						return true;
+					}
 				}
 				t.selectionEnd = Math.min(t.selectionStart + 1, t.value.length);
 				t.selectionStart = Math.min(t.selectionStart, t.value.length - 1);
 				return true;
 			case 40:
 				// down
-				gotoNext();
+				if (next) gotoNext();
 				return true;
 		}
 		return false;
@@ -161,8 +213,9 @@ class DinForm2 {
 		event.target.selectionStart = caret;
 		event.target.selectionEnd = Math.min(caret + 1, s.length);
 		if (event.target.selectionStart >= 6) {
-			this.seqIn.selectionStart = 5;
-			this.seqIn.selectionEnd = this.seqIn.value.length;
+			this.seqIn.blur(); // don't keep typing, want to eat up the extra barcode characters.
+			//this.seqIn.selectionStart = 5;
+			//this.seqIn.selectionEnd = this.seqIn.value.length;
 		}
 	}
 
--- blood/sidebar/main.js
+++ blood/sidebar/main.js
@@ -6,6 +6,37 @@ let phenotypePopupForm;
 let sidebar;
 
 
+let triggerBarcode = '';
+
+function handleBarcodeScan(event) {
+	//console.log(event.type);
+	// barcode inputs probably won't input shift, but teseting 
+	//with a keyboard messes up this function, so ignore shift.
+	if (event.key == 'Shift') return;
+
+	// start looking for ISBT inputs
+	if (event.key == '=' || event.key == '&') {
+		triggerBarcode = event.key;
+		return;
+	}
+
+	// handling second character of ISBT barcode inputs.
+	if (triggerBarcode == '=') {
+		if (event.key == '%') { console.log("set group from barcode"); }
+		else if (event.key == '<') { console.log("set product from barcode"); }
+		else { console.log("set DIN from barcode"); }
+		triggerBarcode = '';
+	}
+	else if (triggerBarcode == '&') {
+		if (event.key == '>') { console.log("set expiry date from barcode"); }
+		else if (event.key == '\\') { console.log("set red cell antigens from barcode"); }
+		else if (event.key == '{') { console.log("set HLA/HPA antigens from barcode"); }
+		else if (event.key == '\"') { console.log("set infectious markers from barcode"); }
+		else if (event.key == '*') { console.log("set collection date from barcode"); }
+		triggerBarcode = '';
+	}
+}
+
 (function init() {
 	// set up tristateBoxes.
 	document.querySelectorAll('.tristateBox').forEach((box) => {
@@ -18,4 +49,8 @@ let sidebar;
 		document.querySelector('#phenotypePopup'),
 		document.querySelector('#phenotypeDisplay')
 	);
+
+	// don't know which it would be.
+	document.querySelector('body').addEventListener('keyup', handleBarcodeScan.bind(event));
+	document.querySelector('body').addEventListener('keydown', handleBarcodeScan.bind(event));
 })();
